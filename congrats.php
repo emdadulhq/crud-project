b91400f00968bd6c57f5b1841535392e3f54fa73
emdadulhq <emdadulhq@gmail.com>
2020-06-30 22:32:49 +0600

    congrats massege with new page

---
+++ congrats.php
@@ -0,0 +1,25 @@
+<?php include_once "apps/function.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Congratulate !!!</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	<div class="wrap shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>Congratulation !!!</h2>
+				<h3 class="bg bg-success text-light"> আপনার প্রদেয় সকল তথ্য সফল ভাবে সংরক্ষন করা হয়েছে। আমাদের সাথে আপনার পরিচয় ভাগ করার জন্য অসংখ্য ধন্যবাদ।:</h3>
+				
+				<a href="index.php" class="btn btn-primary">Add Another Student</a>
+					
+			</div>
+		</div>
+	</div>
+</body>
+</html>
